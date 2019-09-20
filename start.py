3f55fa7f01c79471532cbbafda0c49bf1e18f5d8
covxx <cmjensenx@gmail.com>
2019-09-20 00:03:25 -0400

    Started program work
    Created main menu, started working on input

---
+++ start.py
@@ -0,0 +1,21 @@
+#Main program for MSS 
+
+
+print ("Welcome to Robin Farming Intrustment Launcher ")
+print ("Ver. 0.0.1")
+print ("----------------------------------------------")
+print ("1. launch") # Launch main program
+print ("2. Settings") # Config info
+print ("3. Quit") # Close Program
+print ("----------------------------------------------")
+# MMI = Main Menu Input // Move to next [age 
+mmi = int(input("Enter number selection to proceed:  "))
+#print(mmi) # Test EOF
+if mmi == 1:
+    print ("1 p")
+else:
+    if mmi == 2:
+        print ("2")
+#else:
+ #   if mmi == 3:
+ #       print ("3")
