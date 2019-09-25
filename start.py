61c0ab306840eba7a706af1f16e280dad75b97a8
covxx <cmjensenx@gmail.com>
2019-09-24 21:53:20 -0400

    Created PAGE GUI

--- start.py
+++ start.py
@@ -1,21 +1,23 @@
 #Main program for MSS 
 
+import sys
+import time
+import glob
 
 print ("Welcome to Robin Farming Intrustment Launcher ")
 print ("Ver. 0.0.1")
 print ("----------------------------------------------")
 print ("1. launch") # Launch main program
-print ("2. Settings") # Config info
 print ("3. Quit") # Close Program
 print ("----------------------------------------------")
 # MMI = Main Menu Input // Move to next [age 
 mmi = int(input("Enter number selection to proceed:  "))
 #print(mmi) # Test EOF
 if mmi == 1:
-    print ("1 p")
+    print ("Loading...")
 else:
     if mmi == 2:
-        print ("2")
-#else:
- #   if mmi == 3:
- #       print ("3")
+        print ("Quit")
+
+
+
