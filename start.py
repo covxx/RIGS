2d8c887b520bd63b96b7c24766365235c14a1932
covxx <cmjensenx@gmail.com>
2019-10-17 23:30:41 -0400

    Updated files
    Changelog has changes
    Program v 0.1.5

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
-#Main program for MSS
+#Main program for Robin 
 # Copy right Christian Jensen @covxx
 # Contact cmjensenx@gmail.com
 #
@@ -15,28 +15,29 @@ humi = round(sense.get_humidity())
 mbar = round(sense.get_pressure())
 #All other program setup
 tf = open('TempLog.txt', 'a')
-ctime = datetime.datetime.now()
+#ctime = datetime.datetime.now()
 def get_temp(): #Prints temp and is EOP
     print ("The temperature is currently:" ,temp, "F")
- #   tf.write(sense.temp)
+ #   tf.write(sense.temp) | Starting work on data logging to local FS 
   #  tf.write('\n')
    # tf.writeclose()
     #sleep(50)
-def hum():
+def hum(): #Humidity 
      print ("The humidity is currently:",humi, "%")
      
-def pbar():
+def pbar #Pressure
     print ("The pressure is currently:",mbar,"mbar")
 
 def main_Menu():
     print ("Welcome to Robin Farming Intrustment")
-    print ("Ver. 0.1.0")
-   # print ("The time currently is:"), ctime.strftime("%Y-%M-%D %H:%m:s")
+    print ("Ver. 0.1.5 - 10/17/19")
+   # print ("The time currently is:"), ctime.strftime("%Y-%M-%D %H:%m:s") #Says current time; WIP/ May remove 
     print ("----------------------------------------------")
-    print ("1. Temperature") # Prints Temp
+    print ("1. Temperature") #Prints Temp
     print ("2. Humidity") #Print Humidity
-    print ("3. Pressure") # Pressure
-    print ("3. Quit") # Close Program
+    print ("3. Pressure") #Prints Pressure
+    print ("4. Quit") #Close Program
+    print ("5. Settings") #Program settijgs 
     print ("----------------------------------------------")
     # MMI = Main Menu Input // Get temp or quit
     mmi = int(input("Enter number selection to proceed:  "))
@@ -47,11 +48,20 @@ def main_Menu():
     elif mmi == 2:
         print ("Loading...")
         hum() 
-    elif mmi == 4:
-        print ("Goodbye")
-        sys.exit()
     elif mmi == 3:
         print ("Loading...")
         pbar()
-            
+    elif mmi == 4:
+        print ("Logging out...")
+        #shtdwn() | Shutdown class is currently not implmented 
+        sys.exit()
+    elif mmi == 5:
+        print ("Loading...")
+        settings()
+
+def settings(): #Settings menu, but no settings
+    main_menu() #Goes back to main menu until theres funticans 
+    
+def shtdwn(): #Log out process to close data logging and save to local FS and network
+
 main_Menu() #runs the program
