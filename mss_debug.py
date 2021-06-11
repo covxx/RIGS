294ebce7da4e1c50d184ae33bbb6964c7c4808ce
cxmx <covxx@users.noreply.github.com>
2021-06-11 11:45:02 -0400

    Update mss_debug.py
    Removed settings

--- mss_debug.py
+++ mss_debug.py
@@ -77,28 +77,6 @@ def sm(): #Menu Option Two
     else: #Any other number re-runs menu
         sm()
         main_Menu()
-def setng():
-        print ("Settings Menu")
-        print ("1. Device Config") #Config options
-        print ("2. Data Logging Timer") #Change Data logging end timer
-        print ("3. Testing") #Testing menu
-        print ("4. Main Menu") #Back to main menu
-        print ("----------------------------------------------")
-        seti = int(input("Enter number selection to proceed:  "))
-        if seti == 1:
-            print ("Loading...")
-            #dcon() #Not implemented //Device config
-        elif seti == 2:
-            print ("Loading...")
-            #dlet() #Not implemented //Data Logging end timer
-        elif seti == 3:
-            print ("Loading...")
-            #pbar() #Not implemented //Testing menu
-        elif seti == 4:
-            print("Loading main menu..")
-            main_Menu()
-        else: #Any other number re-runs menu
-            setng()
 def g_pbar():
     #mdls_pbar = round(sense.get_pressure()) #rounds pressure reading
     print("Current Zone pressure Level:", mdls_pbar,"milliebars") #Sensehat unit is milliebars, future will convert
