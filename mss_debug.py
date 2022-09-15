b287f9428ea3de4ac15fa0b223b61234a4116566
cxmx <covxx@users.noreply.github.com>
2022-09-15 17:10:18 -0400

    Update mss_debug.py

--- mss_debug.py
+++ mss_debug.py
@@ -21,55 +21,55 @@ temp = sense.get_temperature()
 #from sense_hat import SenseHat
 #from datetime import datetime
 def cls():
-	system('clear')
+    system('clear')
 def FRun(): #First time run check check, verifies config file is present
-	config_exists = os.path.exists('config.py') 
-	if config_exists == True:
-		print(config_exists) #Debug
-		MainStart()
-	if config_exists == False:
-		ConfigSetup() #Calls program config
-		#print(config_exists) #Debug
+    config_exists = os.path.exists('config.py') 
+    if config_exists == True:
+        print(config_exists) #Debug
+        MainStart()
+    if config_exists == False:
+        ConfigSetup() #Calls program config
+        #print(config_exists) #Debug
 def ConfigSetup(): #Creates config file
-	cls()
-	print('Welcome to RIGS')
-	print('Please wait, preforming first time setup...')
-	with open('config.py', 'w'): pass
-	MainStart()
+    cls()
+    print('Welcome to RIGS')
+    print('Please wait, preforming first time setup...')
+    with open('config.py', 'w'): pass
+    MainStart()
 def MainStart():
-	cls()
-	print('Welcome to RIGS')
-	print('----------------------------------------------')
-	print('1. Start Data Logging')#Run data logging session
-	print("2. Sensor Data") #Live Data From Sensors
-	#print("3. Settings") #Not sure what settings will be needed yet...
-	print('4. Exit') #Close Program
-	print ('----------------------------------------------')
-	msi = int(input('Enter number selection to proceed:  '))
-	if msi == 1:
-		print ("Loading...")
-		Start_DLS() #| In Progress... 
-	elif msi == 2:
-		print ("Loading...")
-		#sm()
-	elif msi == 3:
-		print ("Loading...")
-    	#setng()
-	elif msi == 4: 
-		print ("Shuting down application")
-		exit() #Closes program
-	else:
-		MainStart()
+    cls()
+    print('Welcome to RIGS')
+    print('----------------------------------------------')
+    print('1. Start Data Logging')#Run data logging session
+    print("2. Sensor Data") #Live Data From Sensors
+    #print("3. Settings") #Not sure what settings will be needed yet...
+    print('4. Exit') #Close Program
+    print ('----------------------------------------------')
+    msi = int(input('Enter number selection to proceed:  '))
+    if msi == 1:
+        print ("Loading...")
+        Start_DLS() #| In Progress... 
+    elif msi == 2:
+        print ("Loading...")
+        #sm()
+    elif msi == 3:
+        print ("Loading...")
+        #setng()
+    elif msi == 4: 
+        print ("Shuting down application")
+        exit() #Closes program
+    else:
+        MainStart()
 def Start_DLS(): #General Datalogging, will become menu later with abilty to define args, now for debuging
-	cls()
-	LogCount = 0 #Loop timer
-	LogTime = 0
-	LogTime = int(input('How many seconds do you want to datalog for?	'))
-	while (LogCount < LogTime):   
-		LogCount = LogCount + 1
-		cls()
-		time.sleep(1)
+    cls()
+    LogCount = 0 #Loop timer
+    LogTime = 0
+    LogTime = int(input('How many seconds do you want to datalog for?   '))
+    while (LogCount < LogTime):   
+        LogCount = LogCount + 1
+        cls()
+        time.sleep(1)
         #print(temp)
-	print(LogCount)
+    print(LogCount)
 cls()
 FRun()
