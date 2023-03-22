991f5e23920ac4a2fe14e698cdb8100fa6be83b1
cxmx <covxx@users.noreply.github.com>
2023-03-22 15:39:49 -0400

    update mss.py
    Started automated DLS, some things commented out for debug

--- mss.py
+++ mss.py
@@ -9,11 +9,11 @@ from os import system
 from datetime import datetime
 from datetime import date
 from os import system, name
-from prometheus_client import Gauge, start_http_server #For web server
-web_port = 9090 #Web server port
+#from prometheus_client import Gauge, start_http_server #For web server
+#web_port = 9090 #Web server port
 #start_http_server(web_port) #Web sever start
-gt = Gauge('RIGS_temperature',
-           'Temperature measured by the RIGS Sensor', ['scale'])
+#gt = Gauge('RIGS_temperature',
+#           'Temperature measured by the RIGS Sensor', ['scale'])
 today_date = date.today() #Sets todays date as the current date
 current_date = today_date.strftime("%m_%d_%Y") #Gets date, saves to var
 today_date_time = datetime.now()
@@ -63,6 +63,37 @@ def MainStart():
 		exit() #Closes program
 	else:
 		MainStart()
+def Auto_DLS_Start(): #Automated DLS
+	clear_screen()
+	Auto_dls_temp = 0 #Var for temp, auto hold
+	Auto_LogTime = 0 #Logtime for automated logging, user will need to set 
+	Auto_LogTime_counter = Auto_LogTime #Counter for logging, able to be reset and preserve data
+	Auto_LogInterval = 0 #How long till data logging starts
+	Auto_LogInterval_Counter = Auto_LogInterval #counter
+	Auto_LogCount = 1 #How many times logging will occur, after log time has ended and after log interval is over
+	#NEED INPUT FOR ABOVE VARS - maybe here or somewhere else?
+	while Auto_LogTime_counter > 0:
+		Auto_LogTime_counter = Auto_LogTime_ounter - 1
+		DLS_FileName = ('Temp_Log_' + str(current_date) + '.txt') #Sets log file namee using 'todays' date from var current_date
+		with open(DLS_FileName, "a") as f: #Append data if file exists but will create new if not
+			f.write( str(current_time) + ': The current tempture is: ' + str(Auto_dls_temp) + ' F \n') #Writes current temp to new line with time
+			print('Data logging in progress: ' + str(Auto_LogTime_counter) + (' seconds remaining in session.')) #Prints seconds left of DLS session
+			time.sleep(0.5) #Waits half second before looping
+		if Auto_LogTime_counter == 0: #Once session has finished	
+			Auto_LogCount = Auto_LogCount - 1 #Minus 1 to log count
+			Auto_LogTime_counter = Auto_LogTime_counter + Auto_LogTime
+			while Auto_LogCount > 0:
+				clear_screen()
+				Auto_LogInterval_Counter = Auto_LogInterval_Counter - 1
+				print("Automated data logging session has finished, next session starts in ", Auto_LogInterval, " seconds.")
+				time.sleep(0.5) #Wait
+			if Auto_LogCount == 0:
+				clear_screen()
+				print("Automated data logging session has finished")
+				MainStart()
+
+		
+			
 def Start_DLS(): #General Datalogging, will become menu later with abilty to define args, now for debuging
 	clear_screen()
 	dls_temp = 0 #Current temp
@@ -83,7 +114,7 @@ def Start_DLS(): #General Datalogging, will become menu later with abilty to def
 	print('Data logging session has completed, log saved as ' + DLS_FileName )
 	input('Press enter to return to main menu.')
 	MainStart()
-def Start_sd(): #Live sensor data menu, no logging IN PROGRESS!!
+def Start_sd(): #Live sensor data menu, no logging | IN PROGRESS
 	sd_temp = 0
 	clear_screen()
 	try:
