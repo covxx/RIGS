b398a876546f2b93d50eded571e9538fcd4fd4ef
cxmx <covxx@users.noreply.github.com>
2023-03-21 22:56:00 -0400

    Update mss.py
    Time and date rework, implanting file naming.

--- mss.py
+++ mss.py
@@ -12,15 +12,19 @@ import time
 import os
 from os import system
 from datetime import datetime
-#import os.name
+from datetime import date
+from os import system, name
 #import exists
 #from sense_hat import SenseHat
-today_date = datetime.now()
-current_date = today_date.strftime("%d/%m/%Y %H:%M:%S") #Gets date and saves to var
+today_date = date.today()
+current_date = today_date.strftime("%m_%d_%Y") #Gets date, saves to var
 today_date_time = datetime.now()
-current_date_time = today_date_time.strftime("%d/%m/%Y %H:%M:%S") #Gets date and time and saves to var
-def cls():
-	system('clear')
+current_date_time = today_date_time.strftime("%m/%d/%Y %I:%M%p") #Gets date and time and saves to var in 12 hour format
+def clear_screen():
+    if name == 'nt': #Clear screen for windows
+        _ = system('cls')
+    else: #Clear screen for Mac and Linux
+        _ = system('clear')
 def FRun(): #First time run check check, verifies config file is present
 	config_exists = os.path.exists('config.txt') 
 	if config_exists == True:
@@ -30,15 +34,15 @@ def FRun(): #First time run check check, verifies config file is present
 		ConfigSetup() #Calls program config
 		#print(config_exists) #Debug
 def ConfigSetup(): #Creates config file
-	cls()
+	clear_screen()
 	print('Welcome to RIGS')
 	print('Please wait, preforming first time setup...')
 	with open('config.py', 'w'): pass
 	MainStart()
 def MainStart():
-	cls()
+	clear_screen()
 	print('Welcome to RIGS')
-	print ('Today is', current_date)
+	print ('Today is', current_date_time)
 	print('----------------------------------------------')
 	print('1. Start Data Logging')#Run data logging session
 	print("2. Sensor Data") #Live Data From Sensors
@@ -61,17 +65,22 @@ def MainStart():
 	else:
 		MainStart()
 def Start_DLS(): #General Datalogging, will become menu later with abilty to define args, now for debuging
-	cls()
+	clear_screen()
 	dls_temp = 0 #Current temp
 	LogCount = 0 #Loop timer
 	LogTime = 0
 	LogTime = int(input('How many seconds do you want to datalog for?:	'))
+	LogRemain = LogTime #Time remaining counter
 	while (LogCount < LogTime):
 		LogCount = LogCount + 1
-		cls()
-		with open('test.txt', "a") as f: #Will need to be set to current date will make sure new files are created
-			f.write( str(current_date_time) + ': The current tempture is: ' + str(dls_temp) + ' F \n') #Writes current temp to new line
-			time.sleep(0.5) #Waits half second before looping
+		LogRemain = LogRemain - 1
+		clear_screen()
+		DLS_FileName = (str(current_date))
+		print(DLS_FileName)
+		#with open('DLS_FileName+ '.txt', "a") as f: #Will need to be set to current date will make sure new files are created
+			#f.write( str(current_date_time) + ': The current tempture is: ' + str(dls_temp) + ' F \n') #Writes current temp to new line
+			#print('Data logging in progress ' + str(LogRemain) + (' seconds remaining of session.')) #Prints seconds left of DLS session
+			#time.sleep(0.5) #Waits half second before looping
 	print(LogCount)
-cls()
+clear_screen()
 FRun()
