9fba0e543d5701b95a2266d202929d1316ecabb6
cxmx <covxx@users.noreply.github.com>
2023-03-21 21:45:50 -0400

    Update mss.py

--- mss.py
+++ mss.py
@@ -69,9 +69,9 @@ def Start_DLS(): #General Datalogging, will become menu later with abilty to def
 	while (LogCount < LogTime):   
 		LogCount = LogCount + 1
 		cls()
-		with open('test.txt', "w+"): #Will need to be set to current date
-			with open.write('Tempture is', current_temp):
-				time.sleep(1) #Waits a second before looping, need to make a better way to do this.
+		with open('test.txt', "a") as f: #Will need to be set to current date
+			f.write('Tempture is', current_temp)
+			time.sleep(1) #Waits a second before looping, need to make a better way to do this.
 	print(LogCount)
 cls()
 FRun()
