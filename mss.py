6621aafbc3c5a22da2c4fe10f36c0f7c9fe5210e
cxmx <covxx@users.noreply.github.com>
2023-03-22 17:30:48 -0400

    Update mss.py

--- mss.py
+++ mss.py
@@ -9,6 +9,11 @@ from os import system
 from datetime import datetime
 from datetime import date
 from os import system, name
+Auto_LogTime = 0 #Logtime for automated logging, user will need to set 
+Auto_LogTime_counter = Auto_LogTime #Counter for logging, able to be reset and preserve data
+Auto_LogInterval = 0 #How long till data logging starts
+Auto_LogInterval_Counter = Auto_LogInterval #counter
+Auto_LogCount = 0
 today_date = date.today() #Sets todays date as the current date
 current_date = today_date.strftime("%m_%d_%Y") #Gets date, saves to var
 today_date_time = datetime.now()
@@ -50,7 +55,7 @@ def MainStart():
 		Start_DLS() #Data logging to log file works, need to figure out sensor situation to implemnent actual data instead of static var
 	elif msi == 2:
 		print ("Loading...")
-		Auto_DLS_Start()
+		Auto_DLS_PreStart()
 	elif msi == 3:
 		print ("Loading...")
 		Start_sd()
@@ -59,15 +64,22 @@ def MainStart():
 		exit() #Closes program
 	else:
 		MainStart()
-def Auto_DLS_Start(): #Automated DLS
+def Auto_DLS_PreStart(): #Need to set variables first - could use lists instead
+	clear_screen()
+	print('RIGS Automated Data logging Setup')
+	print('------------------------------------')
+	Auto_LogTime = input('Enter how long would you like to data log for? (in seconds): ')
+	Auto_LogCount = input('Enter how many times would you like to data log? (1-10): ')
+	Auto_LogInterval = input("Enter how long between data logging sessions, in seconds?: ")
+	print(Auto_LogTime)
+	print(Auto_LogCount)
+	print(Auto_LogInterval)
+	Auto_DLS_Start()
+def Auto_DLS_Start(): #Automated DLS, vars from Auto_DLS_PreStart
 	#clear_screen()
-	Auto_dls_temp = 2 #Var for temp, auto hold
-	Auto_LogTime = 15 #Logtime for automated logging, user will need to set 
+	Auto_dls_temp = 0 #Var for temp, auto hold
 	Auto_LogTime_counter = Auto_LogTime #Counter for logging, able to be reset and preserve data
-	Auto_LogInterval = 0 #How long till data logging starts
 	Auto_LogInterval_Counter = Auto_LogInterval #counter
-	Auto_LogCount = 2 #How many times logging will occur, after log time has ended and after log interval is over
-	#NEED INPUT FOR ABOVE VARS - maybe here or somewhere else?
 	while Auto_LogTime_counter > 0:
 		Auto_LogTime_counter = Auto_LogTime_counter - 1
 		DLS_FileName = ('Temp_Log_' + str(current_date) + '.txt') #Sets log file namee using 'todays' date from var current_date
@@ -78,16 +90,19 @@ def Auto_DLS_Start(): #Automated DLS
 		if Auto_LogTime_counter == 0: #Once session has finished	
 			Auto_LogCount = Auto_LogCount - 1 #Minus 1 to log count
 			Auto_LogTime_counter = Auto_LogTime_counter + Auto_LogTime
-			while Auto_LogCount > 0:
+			while Auto_LogCount != 0 and Auto_LogInterval_Counter != 0:
 				#clear_screen()
+				print(str(Auto_LogInterval_Counter))
 				Auto_LogInterval_Counter = Auto_LogInterval_Counter - 1
-				print("Automated data logging session has finished, next session starts in ", Auto_LogInterval, " seconds.")
-				time.sleep(0.5) #Wait
+				print("Automated data logging session has finished, next session starts in ", Auto_LogInterval_Counter, " seconds.")
+				time.sleep(2.0) #Wait
+			if Auto_LogInterval_Counter == 0:
+				Auto_DLS_Start()
 			if Auto_LogCount == 0:
 				#clear_screen()
 				print("Automated data logging session has finished")
-				print(Auto_LogCount) #Debug
-				print(Auto_LogInterval) #Debug
+				print(str(Auto_LogCount)) #Debug
+				print(str(Auto_LogInterval)) #Debug
 				time.sleep(40) #DEBUGGG
 def Start_DLS(): #General Datalogging, will become menu later with abilty to define args, now for debuging
 	clear_screen()
