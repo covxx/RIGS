8cf247fbe4c76695c59b800fa018e2e04bdf14f2
cxmx <covxx@users.noreply.github.com>
2023-03-27 15:48:25 -0400

    Ver 0.5 Update
    Many changes and additions have been done however must notably is GUI implementation for the main menu, GUI code for automated session (not implem at this time due to issues with threading), threading support, and a fully working automated and 'quick' data log session. Next stage is to clean up current code base and GUI progress menu(s)

--- mss.py
+++ mss.py
@@ -10,7 +10,7 @@ from os import system
 from datetime import datetime
 from datetime import date
 from os import system, name
-from tkinter import simpledialog, Tk, Canvas, Entry, Text, PhotoImage, Button
+from tkinter import simpledialog, Tk, Canvas, Entry, Text, PhotoImage, Button, messagebox
 from pathlib import Path
 global comp_usage
 global GUI
@@ -21,12 +21,9 @@ global Auto_LogInterval
 global b_ver
 global Auto_LogCount_save
 global Prog_TRun
-if os.environ.get('DISPLAY','') == '': #DEBUG for SSH testing
-    print('no display found. Using :0.0') #DEBUG for SSH testing
-    os.environ.__setitem__('DISPLAY', ':0.0') #DEBUG for SSH testing
-b_ver = ("v0.5 3_26_2023") #Bulid verison, used for GUI
+b_ver = ("v0.5 3_27_2023") #Bulid verison, used for GUI
 Prog_TRun = 0 #Program total run time (used for threading and progress bar TBD)
-comp_usage = "Palumbo Foods"
+comp_usage = "Palumbo Foods LLC" #Company using
 Auto_LogCount_save = 0
 Auto_TotalRun_Counter = 0
 Auto_LogTime = 0 #Logtime for automated logging, user will need to set 
@@ -39,19 +36,12 @@ current_date = today_date.strftime("%m_%d_%Y") #Gets date, saves to var
 today_date_time = datetime.now()
 current_date_time = today_date_time.strftime("%m/%d/%Y %I:%M%p") #Gets date and time and saves to var in 12 hour format
 current_time = today_date_time.strftime("%I:%M%p")
-#from prometheus_client import Gauge, start_http_server #For web server
-#web_port = 9090 #Web server port
-#start_http_server(web_port) #Web sever start
-#gt = Gauge('RIGS_temperature',
-#           'Temperature measured by the RIGS Sensor', ['scale'])
+if os.environ.get('DISPLAY','') == '': #DEBUG for SSH testing
+    print('no display found. Using :0.0') #DEBUG for SSH testing
+    os.environ.__setitem__('DISPLAY', ':0.0') #DEBUG for SSH testing
 def Thread_ADLS(): #Auto_Data_Logging thread call, so tkinter window and code run
 	t1=Thread(target=Auto_DLS_Start_prog)
 	t1.start()
-def Thread_Test():
-	x = 0
-	while x < 50000:
-		x = x + 10
-		print(x)
 def clear_screen():
     if name == 'nt': #Clear screen for windows
         _ = system('cls')
@@ -109,39 +99,27 @@ def Auto_DLS_PreStart(): #Need to set variables first - could use lists instead
 	global Prog_TRun #Total program run time
 	Auto_dls_temp = 1
 	clear_screen()
-	print('RIGS Automated Data logging Setup')
-	print('------------------------------------')
+	#print('RIGS Automated Data logging Setup')
+	#print('------------------------------------')
+	window.withdraw() #Closes Tkinter window
 	Auto_LogTime = simpledialog.askinteger(title="RIGS Automated Testing",
-				       							prompt="How Long would you like to data log for, in nminutes? ")
-	Auto_LogInterval = simpledialog.askinteger(title="RIGS Automated Testing",
-				       							prompt="Enter how long between data logging sessions, in minutes? ")
+				       							prompt="How Long would you like to data log for, in minutes? ")
 	Auto_LogCount = simpledialog.askinteger(title="RIGS Automated Testing",
 				       							prompt="Enter how many times would you like to data log? ")
-	#Auto_LogTime = int(input('Enter how long would you like to data log for? (in seconds): '))
-	#Auto_LogInterval = int(input("Enter how long between data logging sessions, in seconds?: "))
-	#Auto_LogCount = int(input('Enter how many times would you like to data log? (1-10): '))
-	#print(Auto_LogTime)
-	#print(Auto_LogCount)
-	#print(Auto_LogInterval)
-	#print(Prog_TRun) #DEBUG 9
-	#print("DEBUG 9") #Debug 9
-	#time.sleep(25.0) #Debug 9
+	Auto_LogInterval = simpledialog.askinteger(title="RIGS Automated Testing", #Would like to remove this if not needed but will assume multi sessions for now.
+					       							prompt="Enter how long between data logging sessions, in minutes? ")
 	Auto_LogTime_counter = Auto_LogTime_counter + Auto_LogTime * 60 #Counter for logging, able to be reset and preserve data
 	Auto_LogInterval_Counter = Auto_LogInterval_Counter + Auto_LogInterval * 60 #counter
 	Prog_TRun = Prog_TRun + Auto_LogTime_counter + Auto_LogInterval_Counter * Auto_LogCount
-	print(Auto_LogTime_counter)
-	print(Auto_LogInterval_Counter)
-	print(Prog_TRun)
-	time.sleep(20)
 	Auto_LogCount_save = (Auto_LogCount_save + Auto_LogCount)
 	DLS_FileName = ('Temp_Log_' + str(current_date) + '.txt') #Adds session details to file
 	with open(DLS_FileName, "a") as f: #Append data if file exists but will create new if not
-			f.write("New data logging session start at " + str(current_time) + ': Each test will run for: ' + str(Auto_LogTime) + ' seconds. Tests will run ' + str(Auto_LogInterval) + ' seconds after the last. Total amount of tests to run is ' + str(Auto_LogCount) + ' \n') #Writes sessions details before session starts
+			f.write("** New data logging session start at " + str(current_time) + ' : Each test will run for: ' + str(Auto_LogTime) + ' minute(s). Test(s) will run ' + str(Auto_LogInterval) + ' seconds after the last. Total amount of tests to run is ' + str(Auto_LogCount) + ' ** \n') #Writes sessions details before session starts
 	Auto_DLS_Start() #Lets go
 def Auto_DLS_Start(): #Automated DLS, vars from Auto_DLS_PreStart
 	clear_screen()
 	print("DEBUG 7") #DEBUG DEBUG
-	Thread_ADLS() #DEBUG DEBUG
+	#Thread_ADLS() #DEBUG DEBUG WORKS!
 	print("DEBUG 8") #DEBUG DEBUG
 	global Auto_LogCount #How many tests to run
 	global Auto_LogTime #How long tests run for
@@ -152,129 +130,7 @@ def Auto_DLS_Start(): #Automated DLS, vars from Auto_DLS_PreStart
 	global Auto_dls_temp
 	global Prog_TRun
 	Auto_dls_temp = 5 #Var for temp, auto hold
-	Auto_LogTime_counter = Auto_LogTime #Counter for logging, able to be reset and preserve data
-	Auto_LogInterval_Counter = Auto_LogInterval #counter for how many tests to run
-	canvas = Canvas(
-    window,
-    bg = "#FFFFFF",
-    height = 408,
-    width = 469,
-    bd = 0,
-    highlightthickness = 0,
-    relief = "ridge"
-	)
-	canvas.place(x = 0, y = 0)
-	image_image_1 = PhotoImage(
-	    file=relative_to_assets("image_1.png"))
-	image_1 = canvas.create_image(
-    	234.0,
-    	102.0,
-    	image=image_image_1
-	)
-	canvas.create_rectangle(
-    	132.0,
-    	185.0,
-    	323.0,
-    	186.0,
-    	fill="#000000",
-    	outline="")
-	canvas.create_rectangle(
-    	118.0,
-    	53.0,
-    	348.0,
-    	54.0,
-    	fill="#000000",
-    	outline="")
-	canvas.create_text(
-	    139.0,
-	    393.0,
-	    anchor="nw",
-	    text="Copyright 2023 Christian Jensen",
-    	fill="#000000",
-    	font=("Inter", 12 * -1)
-	)
-	canvas.create_text(
-    	151.0,
-    	375.0,
-    	anchor="nw",
-    	text="Licensed for: " + comp_usage,
-    	fill="#000000",
-    	font=("Inter", 12 * -1)
-	)
-	button_image_1 = PhotoImage(
-    	file=relative_to_assets("button_5.png"))
-	button_1 = Button(
-	    image=button_image_1,
-    	borderwidth=0,
-    	highlightthickness=0,
-    	command=lambda: exit(), #print("Button 1 pressed"),
-    	relief="flat"
-	)
-	button_1.place(
-    	x=185.0,
-    	y=316.0,
-    	width=96.0,
-    	height=23.0
-	)
-	canvas.create_text(
-	    138.0,
-    	36.0,
-    	anchor="nw",
-    	text="Automated data logging software",
-    	fill="#000000",
-    	font=("Inter", 12 * -1)
-	)
-	canvas.create_text(
-	    221.0,
-	    20.0,
-	    anchor="nw",
-	    text="RIGS",
-	    fill="#000000",
-	    font=("Inter", 12 * -1)
-	)
-	canvas.create_text(
-	    7.0,
-	    8.0,
-	    anchor="nw",
-    	text=b_ver,
-	    fill="#000000",
-    	font=("Inter", 12 * -1)
-	)
-	canvas.create_text(
-	    185.0,
-    	198.0,
-    	anchor="nw",
-    	text="PROGRESS_BAR",
-    	fill="#000000",
-    	font=("Inter", 12 * -1)
-	)
-	canvas.create_text(
-	    57.0,
-	    248.0,
-	    anchor="nw",
-	    text="Time Remaining:",
-	    fill="#000000",
-	    font=("Inter", 12 * -1)
-	)
-	canvas.create_text(
-	    57.0,
-	    281.0,
-	    anchor="nw",
-	    text="Test Cycle:",
-	    fill="#000000",
-	    font=("Inter", 12 * -1)
-	)
-	canvas.create_text(
-    	154.0,
-	    168.0,
-    	anchor="nw",
-    	text="Automated Test In Progress",
-    	fill="#000000",
-    	font=("Inter", 12 * -1)
-	)
-	window.resizable(False, False)
-	window.mainloop()
-	time.sleep(Prog_TRun)
+	Auto_DLS_Start_prog()
 def Auto_DLS_Start_prog(): #Function for DLS, needed to seperate for threading on window **CURRENT TESTING**
 	clear_screen()
 	global Auto_LogCount #How many tests to run
@@ -302,14 +158,14 @@ def Auto_DLS_Start_prog(): #Function for DLS, needed to seperate for threading o
 		if Auto_LogTime_counter == 0 and Auto_LogInterval_Counter == 0:
 			#print("DEBUG 5") #DEBUG
 			#time.sleep(3.0) #DEBUG
-			Auto_LogCount = Auto_LogCount - 1 #Minus 1 to log count				Auto_LogTime_counter = Auto_LogTime_counter + Auto_LogTime
+			Auto_LogCount = Auto_LogCount - 1 #Minus 1 to log count	| Auto_LogTime_counter = Auto_LogTime_counter + Auto_LogTime
 			Auto_DLS_Start()
 		if Auto_LogCount == 0:
 			clear_screen()
-			print("All testing has completed, software has ran ", Auto_LogCount_save, "tests. For a complete run time of ", Auto_TotalRun_Counter)
+			print("All testing has completed, software has ran ", Auto_LogCount_save, "tests. For a complete run time of", Prog_TRun,"seconds.")
 			#print("Debug 6")
 			time.sleep(3.0) #DEBUG
-			MainStart()
+			window.deiconify() #Shows window again
 def Start_DLS(): #General Datalogging, will become menu later with abilty to define args, now for debuging
 	clear_screen()
 	dls_temp = 0 #Current temp
@@ -328,7 +184,7 @@ def Start_DLS(): #General Datalogging, will become menu later with abilty to def
 			time.sleep(0.5) #Waits half second before looping
 	clear_screen()
 	print('Data logging session has completed, log saved as ' + DLS_FileName )
-	input('Press enter to return to main menu.')
+	#input('Press enter to return to main menu.')
 	MainStart()
 def Start_sd(): #Live sensor data menu, no logging | IN PROGRESS
 	sd_temp = 0
@@ -398,7 +254,7 @@ button_1 = Button(
     image=button_image_1,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: Auto_DLS_PreStart(), # print("button_1 clicked"),
+    command=lambda: print("button_1 clicked"),
     relief="flat"
 )
 button_1.place(
@@ -413,7 +269,7 @@ button_2 = Button(
     image=button_image_2,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_2 clicked"),
+    command=lambda: Auto_DLS_PreStart(), #print("button_2 clicked"),
     relief="flat"
 )
 button_2.place(
